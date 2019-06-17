4203ec8820314c970e56d587807eaa5d38492cd6
Mia W <rosie1021@yahoo.com>
2019-06-17 16:57:53 -0700

    This is my first commit.

--- cachematrix.R
+++ cachematrix.R
@@ -2,6 +2,7 @@
 
 ## The first function, makeCacheMatrix creates a special "matrix", which is a list containing
 ## a function to
+
 ## 1. set the value of the matrix
 ## 2. get the value of the matrix
 ## 3. set the inverse of the matrix
